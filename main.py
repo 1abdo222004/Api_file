bb1b4d0194771f0759e13610565920a3a72174ef
1abdo222004 <157881653+1abdo222004@users.noreply.github.com>
2024-03-22 05:23:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask,jsonify,request
-from reqeuts import *
+from requests import *
 url ="https://idwyvctdeopxpsomnrmy.supabase.co/rest/v1/admin?select=id&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlkd3l2Y3RkZW9weHBzb21ucm15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4NzUzNTYsImV4cCI6MjAyNjQ1MTM1Nn0.90KRZaat7FuXBIsjuYOiT1-2VmtkdAQwM6pkKt6ZwK0"
 app=Flask(__name__)
 @app.route('/')
